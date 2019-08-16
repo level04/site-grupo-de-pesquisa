0c9eac859153fdc31a3a49311723b514c360fa60
Alice Francener <alicefrancener@gmail.com>
2019-08-16 19:31:26 -0300

    Formatação do código

--- Script/filtrarMembros.js
+++ Script/filtrarMembros.js
@@ -3,6 +3,7 @@
  */
 
 filtrarSelecao("todos");
+
 function filtrarSelecao(c) {
   var x, i;
   x = document.getElementsByClassName("card");
@@ -33,7 +34,7 @@ function removeClasse(element, name) {
   arr2 = name.split(" ");
   for (i = 0; i < arr2.length; i++) {
     while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1); 
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
     }
   }
   element.className = arr1.join(" ");
@@ -43,7 +44,7 @@ function removeClasse(element, name) {
 var btnContainer = document.getElementById("btn-container");
 var btns = btnContainer.getElementsByClassName("btn");
 for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
+  btns[i].addEventListener("click", function () {
     var current = document.getElementsByClassName("active");
     current[0].className = current[0].className.replace(" active", "");
     this.className += " active";
